b1c8a01116e22f22b7e59d1dc97d2aa94935afc3
sn0wstorm20202 <somsubhra20202@gmail.com>
2025-09-04 19:41:02 +0530

    Update widgetSettings.ts

--- packages/backend/convex/private/widgetSettings.ts
+++ packages/backend/convex/private/widgetSettings.ts
@@ -6,84 +6,83 @@ export const upsert = mutation({
     args:{
         
         greetMessage: v.string(),
-         defaultSuggestions: v.object ({
-              suggestion1: v.optional(v.string()),
-              suggestion2: v.optional(v.string()),
-              suggestion3: v.optional(v.string()),
-            }),
-             vapiSettings: v.object({
-             assistantId: v.optional(v.string()),
-             PhoneNumber: v.optional(v.string()),
-    }),
+         defaultSuggestions: v.object({
+            suggestion1: v.optional(v.string()),
+            suggestion2: v.optional(v.string()),
+            suggestion3: v.optional(v.string()),
+        }),
+        vapiSettings: v.object({
+            assistantId: v.optional(v.string()),
+            phoneNumber: v.optional(v.string()),
+        }),
     },
     handler: async(ctx,args) => {
         const identity = await ctx.auth.getUserIdentity();
-            if (identity === null) {
-              throw new ConvexError({
+        if (identity === null) {
+            throw new ConvexError({
                 code: "UNAUTHORIZED",
                 message: "Identity not found",
-              });
-            }
-        
-            const orgId = identity.orgId as string;
-        
-            if (!orgId) {
-              throw new ConvexError({
+                  });
+        }
+
+        const orgId = identity.orgId as string;
+
+        if (!orgId) {
+            throw new ConvexError({
                 code: "UNAUTHORIZED",
                 message: "Organization not found",
               });
-            }
+        }
 
-            const existingwidgetSettings = await ctx.db
-                .query("widgetSettings")
-                .withIndex("by_organization_id", (q) => q.eq("organizationId", orgId))
-                .unique();
+            const existingWidgetSettings = await ctx.db
+            .query("widgetSettings")
+            .withIndex("by_organization_id", (q) => q.eq("organizationId", orgId))
+            .unique();
 
-                if (existingwidgetSettings) {
-                    await ctx.db.patch(existingwidgetSettings._id,{
-                        greetMessage: args.greetMessage,
-                        defaultSuggestions: args.defaultSuggestions,
-                        vapiSettings: args.vapiSettings,
-                    });
-                }else{
-                    await ctx.db.insert("widgetSettings",{
-                        organizationId: orgId,
-                        greetMessage: args.greetMessage,
-                        defaultSuggestions: args.defaultSuggestions,
-                        vapiSettings: args.vapiSettings,
-                    });
-                }
-                
+           if (existingWidgetSettings) {
+            await ctx.db.patch(existingWidgetSettings._id, {
+                greetMessage: args.greetMessage,
+                defaultSuggestions: args.defaultSuggestions,
+                vapiSettings: args.vapiSettings,
+            });
+        } else {
+            await ctx.db.insert("widgetSettings", {
+                organizationId: orgId,
+                greetMessage: args.greetMessage,
+                defaultSuggestions: args.defaultSuggestions,
+                vapiSettings: args.vapiSettings,
+            });
+        }
                 
     },
 });
 
 
 export const getOne = query({
-    args:{},
-    handler: async(ctx) => {
+    args: {},
+    handler: async (ctx) => {
         const identity = await ctx.auth.getUserIdentity();
             if (identity === null) {
-              throw new ConvexError({
+            throw new ConvexError({
                 code: "UNAUTHORIZED",
                 message: "Identity not found",
-              });
-            }
-        
-            const orgId = identity.orgId as string;
-        
-            if (!orgId) {
-              throw new ConvexError({
+                  });
+        }
+
+        const orgId = identity.orgId as string;
+
+        if (!orgId) {
+            throw new ConvexError({
                 code: "UNAUTHORIZED",
                 message: "Organization not found",
               });
             }
 
-            const widgetSettings = await ctx.db
-                .query("widgetSettings")
-                .withIndex("by_organization_id", (q) => q.eq("organizationId", orgId))
-                .unique();
+               const widgetSettings = await ctx.db
+            .query("widgetSettings")
+            .withIndex("by_organization_id", (q) => q.eq("organizationId", orgId))
+            .unique();
 
-                return widgetSettings;
+               return widgetSettings;
     },
 });
